3583d7d4b4f7462e294f4064fc4a74213a2e3e7d
linjingnan-oss <58840695+linjingnan-oss@users.noreply.github.com>
2019-12-13 14:47:22 +0800

    Initial commit

---
+++ dependencies.js
@@ -0,0 +1,87 @@
+module.exports = [
+  {
+    name: 'vue',
+    library: 'Vue',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js'
+  },
+  {
+    name: 'vue-router',
+    library: 'VueRouter',
+    css: '',
+    js: 'https://unpkg.com/vue-router@3.0.6/dist/vue-router.js'
+  },
+  {
+    name: 'vuex',
+    library: 'Vuex',
+    css: '',
+    js: 'https://unpkg.com/vuex@3.1.1/dist/vuex.js'
+  },
+  {
+    name: '@d2-projects/d2-crud',
+    library: 'D2Crud',
+    css: '',
+    js: 'https://cdn.d2.pub/packages/@d2-projects/d2-crud@2.0.5/d2-crud.js'
+  },
+  {
+    name: '@d2-projects/vue-table-export',
+    library: 'VueTableExport',
+    css: '',
+    js: 'https://cdn.d2.pub/packages/@d2-projects/vue-table-export@1.0.1/vue-table-export.js'
+  },
+  {
+    name: '@d2-projects/vue-table-import',
+    library: 'VueTableImport',
+    css: '',
+    js: 'https://cdn.d2.pub/packages/@d2-projects/vue-table-import@1.0.0/vue-table-import.js'
+  },
+  {
+    name: 'axios',
+    library: 'axios',
+    css: '',
+    js: 'https://unpkg.com/axios@0.19.0/dist/axios.min.js'
+  },
+  {
+    name: 'better-scroll',
+    library: 'BScroll',
+    css: '',
+    js: 'https://unpkg.com/better-scroll@1.15.2/dist/bscroll.min.js'
+  },
+  {
+    name: 'echarts',
+    library: 'echarts',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js'
+  },
+  {
+    name: 'v-charts',
+    library: 'VeIndex',
+    css: 'https://cdn.jsdelivr.net/npm/v-charts/lib/style.min.css',
+    js: 'https://cdn.jsdelivr.net/npm/v-charts/lib/index.min.js'
+  },
+  {
+    name: 'element-ui',
+    library: 'ELEMENT',
+    css: 'https://unpkg.com/element-ui@2.13.0/lib/theme-chalk/index.css',
+    js: 'https://unpkg.com/element-ui@2.13.0/lib/index.js'
+  },
+  // https://github.com/heynext/v-contextmenu/pull/72/commits
+  {
+    name: 'v-contextmenu',
+    library: 'VContextmenu',
+    css: 'https://unpkg.com/v-contextmenu/dist/index.css',
+    js: 'https://unpkg.com/v-contextmenu/dist/index.js'
+  },
+  {
+    name: 'vue-grid-layout',
+    library: 'VueGridLayout',
+    css: '',
+    js: 'https://unpkg.com/vue-grid-layout@2.3.4/dist/vue-grid-layout.umd.min.js'
+  },
+  {
+    name: 'highlight.js',
+    library: 'hljs',
+    css: 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/github-gist.min.css',
+    js: 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/highlight.min.js'
+  }
+]
